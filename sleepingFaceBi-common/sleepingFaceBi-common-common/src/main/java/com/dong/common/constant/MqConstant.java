7d0bfa9d8639b9a52a434acc0e902e86a1140469
wangzhendong <wangzhendong20@163.com>
2024-08-13 14:41:24 +0800

    v2.7.2

--- sleepingFaceBi-common/sleepingFaceBi-common-common/src/main/java/com/dong/common/constant/MqConstant.java
+++ sleepingFaceBi-common/sleepingFaceBi-common-common/src/main/java/com/dong/common/constant/MqConstant.java
@@ -92,8 +92,8 @@ public interface MqConstant {
     /**
      * 定时任务
      */
-    public static final String EXCHANGE_DIRECT_TASK = "exchange.direct.task";
-    public static final String ROUTING_TASK_8 = "task.8";
+    String EXCHANGE_DIRECT_TASK = "exchange.direct.task";
+    String ROUTING_TASK_8 = "task.8";
     //队列
-    public static final String QUEUE_TASK_8 = "queue.task.8";
+    String QUEUE_TASK_8 = "queue.task.8";
 }
